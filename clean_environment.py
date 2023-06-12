29c7b36b2a47f1f3006cfee204098dac20c6d535
Oriel.Goel <orielgoel@gmail.com>
2023-06-12 19:52:27 +0300

    added clean env

--- clean_environment.py
+++ clean_environment.py
@@ -1,4 +1,6 @@
 import requests
-
+# Stops the backend server
 requests.get('http://127.0.0.1:5000/stop_server')
-requests.get('http://127.0.0.1:5001/stop_server')
+
+# Stops the frontend server
+# requests.get('http://127.0.0.1:5001/stop_server')
